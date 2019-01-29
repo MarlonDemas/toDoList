3b429373d9fdbc03001e80d344bbb3e31a60cc1e
MarlonDemas <demasmarlon@gmail.com>
2019-01-29 11:24:37 +0200

    Added base files and created a successful connection to the database

---
+++ connect.php
@@ -0,0 +1,11 @@
+<?php
+    include_once('user.php');
+
+    // Connecting to the database
+    $conn = mysqli_connect(DB_Host, DB_User, DB_Pass, DB_Name);
+
+    // Checking if there is a connection
+    if (mysqli_connect_errno()) {
+        echo "Connection Unsuccessful: " . mysqli_connect_errno();
+    }
+?>
