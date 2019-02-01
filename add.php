1b10bcdbfefd6cdf1e2993f27153d7c2f60c4ad6
MarlonDemas <demasmarlon@gmail.com>
2019-02-01 12:06:45 +0200

    Created a table to display the data in the table

--- add.php
+++ add.php
@@ -5,7 +5,7 @@
                             (1, 'Finish project', 0),
                             (3, 'Do class exercise', 1),
                             (2, 'Do some research on what you would like the project to look like', 1),
-                            (4, 'Have some lunch', 1)");
+                            (4, 'Have some lunch', 0)");
 
     if($add === TRUE) {
         echo 'Table successfully created. You can now <a href="disp.php">display</a> the data in a table.';
--- disp.php
+++ disp.php
@@ -1,10 +1,45 @@
 <?php
     require_once "connect.php";
-
-
-    $todo_res = $db_server->query("SELECT * FROM list_items ORDER BY ListItemID DESC");
-
-    while($row = $todo_res->fetch_assoc()) {
-        echo "<br>" .$row['ListText']. "<br>";
-    };
 ?>
+
+<!DOCTYPE html>
+<html lang="">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Display Table</title>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.4.0/css/bulma.min.css"/>
+    </head>
+    <body>
+        <table class="table is-bordered">
+            <thead>
+                <tr>
+                    <td>ListID</td>
+                    <td>ListText</td>
+                    <td>ListItemDone</td>
+                    <td>ListItemID</td>
+                </tr>
+            </thead>
+            <tbody>
+            <?php
+                $todo_res = $db_server->query("SELECT * FROM list_items ORDER BY ListItemID DESC");
+                while($row = $todo_res->fetch_assoc()) {
+            ?>
+                <tr>
+                    <td><?php echo $row['ListID'] ?></td>
+                    <td><?php echo $row['ListText'] ?></td>
+                    <td><?php echo $row['ListItemDone'] ?></td>
+                    <td><?php echo $row['ListItemID'] ?></td>
+                </tr>
+            <?php } ?>
+            </tbody>
+        </table>
+
+        <?php
+            mysqli_close($db_server);
+        ?>
+
+        <a href="index.php" class="button is-primary">Home</a>
+    </body>
+</html>
